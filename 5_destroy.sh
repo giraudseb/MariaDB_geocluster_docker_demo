14803c44812b3af154599c664befb57f15d559cc
Seb <seb@mariadb.com>
2026-05-11 11:31:03 +0200

    Add Second MaxScale server

--- 5_destroy.sh
+++ 5_destroy.sh
@@ -4,5 +4,5 @@
 #
 . .secret
 
-docker container stop mariadb1 mariadb2 mariadb3 mariadb4 mariadb5 mariadb6 maxscale1 
-docker container rm mariadb1 mariadb2 mariadb3 mariadb4 mariadb5 mariadb6 maxscale1
+docker container stop mariadb1 mariadb2 mariadb3 mariadb4 mariadb5 mariadb6 maxscale1 maxscale2 
+docker container rm mariadb1 mariadb2 mariadb3 mariadb4 mariadb5 mariadb6 maxscale1 maxscale2
